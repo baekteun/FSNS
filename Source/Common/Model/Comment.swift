99c336e997a0e6171a9f2d1bb6dab1df0a48f5aa
baegteun <baegteun@gmail.com>
2021-12-21 14:06:55 +0900

    [Feat] Model MainPageModel, Comment 모델 생성

---
+++ Source/Common/Model/Comment.swift
@@ -0,0 +1,13 @@
+//
+//  Comment.swift
+//  Fashion
+//
+//  Created by 최형우 on 2021/12/21.
+//  Copyright © 2021 baegteun. All rights reserved.
+//
+
+struct Comment{
+    let userName: String
+    let profileImageUrl: String
+    let starCount: Int
+}
---
+++ Source/Common/Model/MainPageModel.swift
@@ -0,0 +1,17 @@
+//
+//  MainPageModel.swift
+//  Fashion
+//
+//  Created by 최형우 on 2021/12/21.
+//  Copyright © 2021 baegteun. All rights reserved.
+//
+
+struct MainPageModel{
+    let userProfileImageUrl: String
+    let userName: String
+    let tag: [String]
+    let postImageUrl: String
+    let hanggerdCount: Int
+    let commentCount: Int
+    let comments: [Comment]
+}
